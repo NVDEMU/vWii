90876b398077d805544278cf7bad82fc93992963
NVDEMU <ndelpo318@gmail.com>
2026-09-25 11:08:29 -0400

    Add Video Interface hardware model

---
+++ src/hardware/video_interface.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+
+namespace vwii::hardware {
+
+class Hollywood;
+
+class VideoInterface {
+public:
+    explicit VideoInterface(Hollywood& hollywood);
+
+    void Reset();
+    void Tick(uint64_t cycles);
+
+    [[nodiscard]] uint32_t Read32(uint32_t address) const;
+    void Write32(uint32_t address, uint32_t value);
+
+    [[nodiscard]] uint64_t FrameCount() const { return frame_count_; }
+    void VBlank();
+
+private:
+    static constexpr uint32_t Base = 0xCC002000;
+    static constexpr uint32_t Size = 0x100;
+
+    Hollywood& hollywood_;
+    std::array<uint32_t, Size / 4> registers_{};
+    uint64_t frame_count_{};
+    uint64_t cycle_accumulator_{};
+};
+
+} // namespace vwii::hardware
