a5ab580e7f08a95588c5f03a0e908bb984cd4b64
NVDEMU <ndelpo318@gmail.com>
2026-09-25 10:48:23 -0400

    Expand PowerPC state with exceptions and system registers

--- src/cpu/powerpc.h
+++ src/cpu/powerpc.h
@@ -19,22 +19,46 @@ public:
     [[nodiscard]] uint32_t GetPC() const { return pc_; }
     [[nodiscard]] uint32_t GetGPR(unsigned index) const { return gpr_.at(index & 31U); }
     [[nodiscard]] uint32_t GetLR() const { return lr_; }
+    [[nodiscard]] uint32_t GetCTR() const { return ctr_; }
     [[nodiscard]] uint32_t GetCR() const { return cr_; }
+    [[nodiscard]] uint32_t GetXER() const { return xer_; }
+    [[nodiscard]] uint32_t GetMSR() const { return msr_; }
+    [[nodiscard]] uint32_t GetSRR0() const { return srr0_; }
+    [[nodiscard]] uint32_t GetSRR1() const { return srr1_; }
     [[nodiscard]] bool Halted() const { return halted_; }
 
 private:
+    static constexpr uint32_t MSR_EE = 0x00008000;
+    static constexpr uint32_t MSR_PR = 0x00004000;
+    static constexpr uint32_t MSR_FP = 0x00002000;
+    static constexpr uint32_t MSR_ME = 0x00001000;
+    static constexpr uint32_t MSR_IR = 0x00000020;
+    static constexpr uint32_t MSR_DR = 0x00000010;
+
     void Execute(uint32_t instruction, uint32_t cia);
+    void RaiseException(uint32_t vector, uint32_t reason);
 
     void SetCR0FromResult(uint32_t value);
+    void SetCRField(unsigned field, unsigned value);
+    [[nodiscard]] unsigned GetCRBit(unsigned bit) const;
+    [[nodiscard]] bool ConditionBit(unsigned bo, unsigned bi) const;
+
     [[nodiscard]] uint32_t ReadBaseRegister(unsigned index) const;
+    [[nodiscard]] uint32_t ReadSPR(unsigned spr) const;
+    void WriteSPR(unsigned spr, uint32_t value);
 
     memory::Memory& memory_;
     std::array<uint32_t, 32> gpr_{};
+
     uint32_t pc_{0x80000000};
     uint32_t lr_{};
     uint32_t ctr_{};
     uint32_t xer_{};
     uint32_t cr_{};
+    uint32_t msr_{};
+    uint32_t srr0_{};
+    uint32_t srr1_{};
+
     bool halted_{};
 };
 
